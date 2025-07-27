0c3de25e9e8d98c50d64839920f8837d1fb1c1d9
mdanikislam <anikislam487@gmail.com>
2025-07-27 21:01:04 +0600

    testimonial

---
+++ src/Component/Testomonial.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Testomonial = () => {
+  return (
+    <div>Testomonial
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas quas ducimus nam eos? Nobis reprehenderit quidem a impedit molestiae delectus maxime laborum eaque repudiandae in sit, officiis laboriosam. Magnam, tempore eum alias assumenda omnis voluptatem voluptatibus repellendus, ex incidunt, rerum rem. Facilis accusamus cumque tempore sed velit est, minima voluptate.
+    </div>
+  )
+}
+
+export default Testomonial
