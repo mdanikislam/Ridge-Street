812fef7d8d0b49bf9860ba581609a7da4c9fc076
mdanikislam <anikislam487@gmail.com>
2025-07-31 23:59:00 +0600

    lorem 3

--- src/Component/Testomonial.jsx
+++ src/Component/Testomonial.jsx
@@ -9,6 +9,8 @@ const Testomonial = () => {
         Lorem ipsum  Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio distinctio cumque exercitationem in ipsa quisquam velit quae est eos sint iure, porro eius similique dolores deleniti assumenda quas, voluptatibus dicta quod eveniet officiis fugiat qui commodi. Quas magnam sint eveniet veniam, consequatur eaque in accusantium recusandae sapiente, illo aut saepe?
 
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem possimus animi laudantium facilis rem perspiciatis aperiam earum inventore. Id, illum?
+
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet illum similique nihil nostrum at saepe suscipit corporis obcaecati molestiae quibusdam incidunt ut harum asperiores, provident fugit id? Distinctio, nobis dolore.
     </div>
   )
 }
